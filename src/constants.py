4818535371c040ec04887247aad210ee694fe732
cheumeni-commit <jeanmichelcheumeni@MacBook-Air-de-Jean.local>
2021-12-09 01:00:57 +0100

    Add new constants

--- src/constants.py
+++ src/constants.py
@@ -6,3 +6,13 @@ C_PARAMETER = "parcelParameter"
 C_COLOR = "color"
 C_BARCODE = "barcode"
 C_CONFBAY = "confBay"
+
+C_PARCEL_COLOR = 'parcelColor'
+C_BARCODE = 'barcode'
+C_DESTBAY = 'destBay'
+C_POSTCODE = 'postCode' 
+C_PARCELID = 'parcelID'
+C_TIMESTAMP = 'timestamp'
+
+C_BAY = 'bay'
+C_UOOO = 'U000'
