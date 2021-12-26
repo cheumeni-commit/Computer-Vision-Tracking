228aadc04504b92ea7b7bb68d4b30f5eed77adda
Jean-Michel CHEUMENI <82422390+cheumeni-commit@users.noreply.github.com>
2021-12-26 22:23:55 +0100

    Delete featuresExtractor-checkpoint.py
    duplicate file

--- src/libs/features/.ipynb_checkpoints/featuresExtractor-checkpoint.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-
-def setParcelsWidthRef(parcels, trackerSpace):
-    for parcel in parcels:
-        if parcel.relativeBox[1] < trackerSpace.imageCenter[0] \
-           and parcel.relativeBox[3] > trackerSpace.imageCenter[0]:
-            parcel.setWidthRef() 
